8d805b9a0eada1144df3145099cb46518faf9dba
Veaceslav Doina <20563034+veaceslavdoina@users.noreply.github.com>
2021-10-17 20:27:47 +0300

    Fixed push issue

--- .codebuild/docker-push.sh
+++ .codebuild/docker-push.sh
@@ -4,11 +4,12 @@
 repository=$1
 
 # Push Docker images to the repository
+now=`date +%Y-%m-%d--%H-%M-%S`
 for image in relay_nginx relay_web relay_worker relay_scheduler
 do
   docker tag $image:latest $repository-$image:latest
   docker push $repository-$image:latest
-  docker tag $image:latest $repository-$image:`date +%Y-%m-%d--%H-%M-%S`
-  docker push $repository-$image:`date +%Y-%m-%d--%H-%M-%S`
+  docker tag $image:latest $repository-$image:$now
+  docker push $repository-$image:$now
 done
 
